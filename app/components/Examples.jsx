ac4a27faa30f58df8de31aa241ea29e3d89e80ce
toronbolo <yorknew45@gmail.com>
2020-01-07 19:58:05 -0400

    added a div

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,9 @@ var React = require('react');
 
 var Examples = (props) => {
     return(
-        <h3>Examples Component</h3>
+        <div>
+            <h3>Welcome to examples page</h3>
+        </div>
     )
 }
 
