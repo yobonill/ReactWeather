0fdd8d2374110a2cd76199841b89aa6ed35f9cf7
toronbolo <yorknew45@gmail.com>
2020-01-07 23:05:25 -0400

    added jquery foundation css-loader style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -11,7 +11,6 @@ var Weather = React.createClass({
     },
     handleSearch: function(location){
         var that = this;
-        debugger;
         this.setState({isLoading: true});
 
         openWeatherMap.getTemp(location).then(function (temperature){
